5583decea978ab1981f98c587359611ca40ab03a
7eezi <46866623+7eezi@users.noreply.github.com>
2019-01-31 22:33:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,27 @@
+const prefix ="!";
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
- console.log("Bot Online"); 
-console.log("log");
+
+
+majd.on('message', function(msg) {
+if(msg.content.startsWith (prefix  + 'server')) {
+ let embed = new Discord.RichEmbed()
+ .setColor('RANDOM')
+ .setThumbnail(msg.guild.iconURL)
+ .setTitle(`Showing Details Of  **${msg.guild.name}*`)
+ .addField('🌐** نوع السيرفر**',`[** __${msg.guild.region}__ **]`,true)
+ .addField('🏅** __الرتب__**',`[** __${msg.guild.roles.size}__ **]`,true)
+ .addField('🔴**__ عدد الاعضاء__**',`[** __${msg.guild.memberCount}__ **]`,true)
+ .addField('🔵**__ عدد الاعضاء الاونلاين__**',`[** __${msg.guild.members.filter(m=>m.presence.status == 'online').size}__ **]`,true)
+ .addField('📝**__ الرومات الكتابية__**',`[** __${msg.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
+ .addField('🎤**__ رومات الصوت__**',`[** __${msg.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
+ .addField('👑**__ الأونـر__**',`**${msg.guild.owner}**`,true)
+ .addField('🆔**__ ايدي السيرفر__**',`**${msg.guild.id}**`,true)
+ .addField('📅**__ تم عمل السيرفر في__**',msg.guild.createdAt.toLocaleString())
+ msg.channel.send({embed:embed});
+}
 });
-
-
- 
-
- 
-client.on('ready', () => { //playing
-    client.user.setGame(`مياوو سبيدي الحيلوااهه هيناا(:	`,'https://www.twitch.tv/faresgameryt');
-    client.user.setStatus('Online')
-});
- 
  
  
  
