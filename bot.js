e3abad0b211e940bf21b6eb9371b0cf04c27ed7e
7eezi <46866623+7eezi@users.noreply.github.com>
2019-01-31 22:31:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,51 +1,21 @@
 const Discord = require('discord.js');
-const initcmd = settings.initcmd;
-const id = settings.id;
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Hi ${client.user.tag} , This Code by : Speedy `);
-  console.log('')
-  console.log(`i Have  [ " ${client.guilds.size} " ]`);
-});
-
-const developers = id
-const adminprefix = initcmd;
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'stplay')) {
-    client.user.setGame(argresult);
-      message.channel.send("**:white_check_mark: | The Playing Status Has Been Changed To : ``"
-   + `${argresult}` + "``**")
-  } else 
-  if (message.content.startsWith(adminprefix + 'stwat')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send("**:white_check_mark: | The Watching Status Has Been Changed To : ``"
-   + `${argresult}` + "``**")
-  } else 
-  if (message.content.startsWith(adminprefix + 'stlis')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send("**:white_check_mark: | The Listening Status Has Been Changed To : ``"
-   + `${argresult}` + "``**")
-  } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/7eezi");
-      message.channel.send("**:white_check_mark: | The Streaming Status Has Been Changed To : ``"
-   + `${argresult}` + "``**")
-  }
-  if (message.content.startsWith(adminprefix + 'stname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'stavatar')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
-}
+ console.log("Bot Online"); 
+console.log("log");
 });
 
 
+ 
 
-
-client.login(process.env.BOT_TOKEN);
+ 
+client.on('ready', () => { //playing
+    client.user.setGame(`مياوو سبيدي الحيلوااهه هيناا(:	`,'https://www.twitch.tv/faresgameryt');
+    client.user.setStatus('Online')
+});
+ 
+ 
+ 
+ 
+ client.login(process.env.TOKEN);
